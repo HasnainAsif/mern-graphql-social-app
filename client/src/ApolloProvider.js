cf02a7d7a6e6706f1f3ffc9ab1638cbc0d6d7892
Hasnain Asif <hasnainasif52@gmail.com>
2023-08-18 19:43:55 +0500

    error link is added in apollo provider for client

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -5,23 +5,48 @@ import {
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
+  ApolloLink,
+  HttpLink,
 } from '@apollo/client';
 import { setContext } from 'apollo-link-context';
+import { onError } from 'apollo-link-error';
 
-const authorizationLink = setContext((request, previousContext) => {
-  const token = localStorage.getItem('jwtToken');
-  return {
-    headers: { Authorization: token ? `Bearer ${token}` : `` },
-  };
-});
+const errorLink = onError(
+  ({ graphQLErrors, networkError, operation, forward }) => {
+    if (graphQLErrors) {
+      // do something with graphql error.
+      // we can add toast errors
+      graphQLErrors.forEach(({ message, locations, path }) =>
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+        )
+      );
+    }
+    if (networkError) {
+      // do something with network error
+      // we can add toast errors
+      console.log(`[Network error]: ${networkError}`);
+    }
+  }
+);
 
-const httpLink = createHttpLink({
-  uri: 'http://localhost:5000',
-});
+const link = () => {
+  const authorizationLink = setContext((request, previousContext) => {
+    const token = localStorage.getItem('jwtToken');
+    return {
+      headers: { Authorization: token ? `Bearer ${token}` : `` },
+    };
+  });
+  const httpLink = createHttpLink({
+    uri: 'http://localhost:5000',
+  });
+
+  const authenticatedLink = authorizationLink.concat(httpLink); // if we donot need errorLink, it can also be returned
+  return ApolloLink.from([errorLink, authenticatedLink]); // httpLink is terminating link and it must be in last.
+};
 
 const client = new ApolloClient({
-  // uri: authorizationLink.concat("http://localhost:5000"),
-  link: authorizationLink.concat(httpLink),
+  link: link(),
   cache: new InMemoryCache(),
 });
 
